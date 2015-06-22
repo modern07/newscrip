2f8b820e73b5bfa2a5d73ff81750fd7b98e242af
Prince Singh <prince.singh@sap.com>
2015-06-22 11:16:35 +0530

    checkin fbscript1

---
+++ scripttool/src/facebook/fb.java
@@ -0,0 +1,32 @@
+package facebook;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class fb {
+	WebDriver driver;
+	
+@BeforeTest
+public void test1()
+{
+	driver=new FirefoxDriver();
+	driver.get("http://www.facebook.com");
+	
+}
+@Test
+public void test2()
+{
+	driver.manage().window().maximize();
+	driver.findElement(By.id("Email")).sendKeys("pricce");
+}
+@AfterTest
+public void test3()
+{
+	driver.quit();
+}
+
+}
